cf4b79a2543afdb6877bc9d693bb69d791e6f747
rakuishi <rakuishi@gmail.com>
2015-05-02 19:17:57 +0900

    Add BlogFragment

---
+++ app/src/main/java/com/rakuishi/ok/ui/MainActivity.java
@@ -0,0 +1,19 @@
+package com.rakuishi.ok.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import com.rakuishi.ok.R;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new BlogFragment())
+                .commit();
+    }
+}
